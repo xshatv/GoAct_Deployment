f112c603317fd623c1f566d7febc162120a6beb5
Akshat Vishwamitra <151455425+xshatv@users.noreply.github.com>
2024-06-10 01:57:22 +0530

    Update main.go

--- backend/main.go
+++ backend/main.go
@@ -1,37 +1,41 @@
 package main
 
 import (
-	"employees/controller"
-	"employees/repository"
-	"employees/routes"
-	"employees/service"
-	"fmt"
-	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/cors"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
-	"log"
-	"os"
+        "employees/controller"
+        "employees/repository"
+        "employees/routes"
+        "employees/service"
+        "fmt"
+        "github.com/gofiber/fiber/v2"
+        "github.com/gofiber/fiber/v2/middleware/cors"
+        "gorm.io/driver/postgres"
+        "gorm.io/gorm"
+        "log"
+        "os"
 )
 
 func main() {
 
-	app := fiber.New()
-	app.Use(cors.New())
-	app.Use(cors.New(cors.Config{
-		AllowOrigins: os.Getenv("ALLOWED_ORIGINS"),
-		AllowHeaders: "Origin, Content-Type, Accept",
-	}))
-	db := initializeDatabaseConnection()
-	repository.RunMigrations(db)
-	employeeRepository := repository.NewEmployeeRepository(db)
-	employeeService := service.NewEmployeeService(employeeRepository)
-	employeeController := controller.NewEmployeeController(employeeService)
-	routes.RegisterRoute(app, employeeController)
-	err := app.Listen(":8080")
-	if err != nil {
-		log.Fatalln(fmt.Sprintf("error starting the server %s", err.Error()))
-	}
+        app := fiber.New()
+//      app.Use(cors.New())
+        allowOrigins := os.Getenv("ALLOWED_ORIGINS")
+        if allowOrigins == "" {
+                allowOrigins = "*" // Fallback to allow all origins
+        }
+        app.Use(cors.New(cors.Config{
+                AllowOrigins: allowOrigins,
+                AllowHeaders: "Origin, Content-Type, Accept",
+        }))
+        db := initializeDatabaseConnection()
+        repository.RunMigrations(db)
+        employeeRepository := repository.NewEmployeeRepository(db)
+        employeeService := service.NewEmployeeService(employeeRepository)
+        employeeController := controller.NewEmployeeController(employeeService)
+        routes.RegisterRoute(app, employeeController)
+        err := app.Listen(":8080")
+        if err != nil {
+                log.Fatalln(fmt.Sprintf("error starting the server %s", err.Error()))
+        }
 }
 func initializeDatabaseConnection() *gorm.DB {
 	db, err := gorm.Open(postgres.New(postgres.Config{
@@ -41,10 +45,8 @@ func initializeDatabaseConnection() *gorm.DB {
 	if err != nil {
 		log.Fatalln(fmt.Sprintf("error connecting with database %s", err.Error()))
 	}
-
 	return db
 }
-
 func createDsn() string {
 	dsnFormat := "host=%s user=%s password=%s dbname=%s port=%s sslmode=disable"
 	dbHost := os.Getenv("DB_HOST")
