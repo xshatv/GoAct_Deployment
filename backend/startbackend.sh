759acb9b61714bb83a37ae0d58ab9672db20d6b5
Akshat Vishwamitra <151455425+xshatv@users.noreply.github.com>
2024-06-18 10:45:24 +0530

    Update startbackend.sh

--- backend/startbackend.sh
+++ backend/startbackend.sh
@@ -4,7 +4,7 @@
 go get ./...
 
 # Run the Go application
-DB_HOST=postgresql1.cnlcckmmoogp.ap-south-1.rds.amazonaws.com DB_USER=postgres DB_PASSWORD=admin123 DB_NAME=postgres DB_PORT=5432 go run main.go
+DB_HOST=postgresql.cepswcsgljye.us-east-1.rds.amazonaws.com DB_USER=postgres DB_PASSWORD=admin123 DB_NAME=postgres DB_PORT=5432 go run main.go
 # DB_HOST=postgresql1.cnlcckmmoogp.ap-south-1.rds.amazonaws.com DB_USER=postgres DB_PASSWORD=admin123 DB_NAME=postgres DB_PORT=5432 ALLOWED_ORIGINS=http://localhost:3000 go run main.go
 
 # Keep the container running
