cb6c8a264f5ca4be10097c5569fb0339e37fc227
Akshat Vishwamitra <151455425+xshatv@users.noreply.github.com>
2024-06-18 15:30:22 +0530

    Update port-forward.sh

--- port-forward.sh
+++ port-forward.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 # Function to handle termination of background processes
-terminate() {
-    echo "Terminating port-forward processes..."
-    kill $(pgrep -f "kubectl port-forward")
+#terminate() {
+#    echo "Terminating port-forward processes..."
+#    kill $(pgrep -f "kubectl port-forward")
     # kill $(jobs -p)
-}
+#}
 
 # Trap SIGINT and SIGTERM to ensure proper cleanup
-trap terminate SIGINT SIGTERM
+#trap terminate SIGINT SIGTERM
 
 # Port-forward commands
-kubectl port-forward --address 0.0.0.0 service/frontend-service 3000:3000> port-forward-frontend.log 2>&1 &
+kubectl port-forward --address 0.0.0.0 service/frontend-service 3000:3000> port-forward-frontend.log &
 echo "Started port-forward for frontend-service:3000 -> 3000"
 
-kubectl port-forward --address 0.0.0.0 service/backend-service 8080:8080 > port-forward-backend.log 2>&1 &
+kubectl port-forward --address 0.0.0.0 service/backend-service 8080:8080 > port-forward-backend.log &
 echo "Started port-forward for backend-service:8080 -> 8080"
 
 # Wait for all background jobs to complete
-wait
+# wait
